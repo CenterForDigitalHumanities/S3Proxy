1c724129b8cf0dc3adb9a6b92e3bad22d8deb0e4
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-07-18 13:23:45 -0500

    Specifically set safe tempfile path

--- src/main/java/servlets/UploadFileToS3Bucket.java
+++ src/main/java/servlets/UploadFileToS3Bucket.java
@@ -7,6 +7,7 @@ package servlets;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -48,34 +49,35 @@ public class UploadFileToS3Bucket extends HttpServlet {
             System.out.println("S3 UploadFileToS3Bucket.java");
 
             Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
-            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
             String fileType = filePart.getContentType();
-            System.out.println();
+            String tmpdir = System.getProperty("java.io.tmpdir") + FileSystems.getDefault().getSeparator();
+            String fileName = tmpdir + Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+            
+            System.out.println("java.io.tmpdir is: " + tmpdir);
             System.out.println("Got file name...");
             System.out.println(fileName);
 
             File tempFile = new File(fileName);
-
-            System.out.println("Made temporary File object...");
-            String tmpdir = System.getProperty("java.io.tmpdir");
-            System.out.println("Temp file path: " + tmpdir);
-
-            System.out.println("Getting the InputStream...");
+            System.out.println("Made File object from filename");
+            System.out.println("File object absolute path is: "+tempFile.getAbsolutePath());
+            
+            System.out.println("Getting the InputStream from filePart from request...");
             InputStream fis = filePart.getInputStream(); 
             System.out.println("We got the input stream!");
 
             System.out.println("Copying input stream to temp file with FileUtils...");
             FileUtils.copyInputStreamToFile(fis, tempFile);
             System.out.println("Populated temp file with contents!");
-       
+            System.out.println(tempFile.getAbsolutePath());
+            
             //CompletedUpload up = bucket.uploadFile(filePart);
-            CompletedUpload up = bucket.uploadFile(tempFile, fileType);
+            //CompletedUpload up = bucket.uploadFile(tempFile, fileType);
 
-            System.out.println("Got completed upload back!  See Etag below, will exist if call was successful.");
-            System.out.println(up.response().eTag());
+            //System.out.println("Got completed upload back!  See Etag below, will exist if call was successful.");
+            //System.out.println(up.response().eTag());
 
             //Note the stuff above is not optimized.  This leaves behind temp files in /CLASSPATHROOT/, they need to be removed.  We might not to generate temp files at all.
-            tempFile.delete();     
+            //tempFile.delete();     
 
             //Not sure what content type this should be yet...we are sending a PutObjectResponse back
             response.setHeader("Content-Type", "text/plain; charset=utf-8");
@@ -84,7 +86,8 @@ public class UploadFileToS3Bucket extends HttpServlet {
             response.setHeader("Access-Control-Expose-Headers", "*");
             response.setHeader("Access-Control-Allow-Methods", "*");
             response.setHeader("Location", Constant.S3_URI_PREFIX + fileName);
-            response.getWriter().print(up.response());
+            response.getWriter().print("***** DONE *******");
+            //response.getWriter().print(up.response());
         }
         
         catch(Exception e){
